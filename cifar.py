c05318f61d68d612367cff7e619a320b4204b0ad
Trace Russell <trace.russell92@gmail.com>
2016-12-05 20:49:10 -0500

    Change CIFAR z_size to 128 to match the ALI paper

--- cifar.py
+++ cifar.py
@@ -6,7 +6,7 @@ class CIFAR(mnist.MNIST):
     def __init__(self):
         self.data = cifar_dataset.read_data_sets('CIFAR_data')
 
-        self.z_size = 64
+        self.z_size = 128
         self.x_size = 32*32
         self.ch = 3
         self.real_images = tf.placeholder(tf.float32, shape=[None, 32, 32, 3])
