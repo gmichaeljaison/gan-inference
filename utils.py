5de78c59a0db1adbd84c2e21fd5fa09cc74ec7c2
Trace Russell <trace.russell92@gmail.com>
2016-12-06 21:53:37 -0500

    Fix image_grid

--- utils.py
+++ utils.py
@@ -6,14 +6,16 @@ def image_grid(images, size):
     fig = plt.figure()
     grid = ImageGrid(fig, 111, nrows_ncols=size, axes_pad=0.1)
     side = int(images.shape[1])
-    if images.ndim == 3:
-        ch = 1
+    ch = images.shape[3]
+    if ch == 3:
+        cmap = None
     else:
-        ch = 3
+        cmap = 'gray'
     for i in xrange(size[0]*size[1]):
-        im = np.reshape(images[i], (side,side,ch))
+        #im = np.reshape(images[i], (side,side,ch))
+        im = np.squeeze(images[i])
         axis = grid[i]
         axis.axis('off')
-        axis.imshow(im, cmap='gray')
+        axis.imshow(im, cmap=cmap)
     return fig
 
